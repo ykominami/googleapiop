9f14af8d15fa68b466c771f212d58b5625ff4ccc
ykominami <ykominami@gmail.com>
2022-08-25 21:56:20 +0900

    first commit

---
+++ lib/googleapiop.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "googleapiop/version"
+require_relative "googleapiop/googleapiop"
+
+module Googleapiop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/googleapiop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Googleapiop do
+  it "has a version number" do
+    expect(Googleapiop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
